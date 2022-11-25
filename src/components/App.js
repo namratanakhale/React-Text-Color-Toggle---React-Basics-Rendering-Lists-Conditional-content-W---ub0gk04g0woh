ea8d11998d08fc2248baf3e79ca92cafe0047472
Namrata Prakash Nakhale <114344082+namratanakhale@users.noreply.github.com>
2022-11-25 11:17:00 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react'
+import '../styles/App.css';
+
+const App = () => {
+//code here 
+  return (
+    <div id="main">
+      <p className={} >Newton School</p>
+      <button id='button' onClick={}>Change Style</button>
+    </div>
+  )
+}
+
+
+export default App;
