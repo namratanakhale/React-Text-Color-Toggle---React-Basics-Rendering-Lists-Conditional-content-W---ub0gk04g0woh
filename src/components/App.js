5e86bb29426f55e9588e8d7f00b68801b52d9201
namratanakhale <namratanakhale91@gmail.com>
2022-11-25 23:27:57 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,22 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+
 const[style, setStyle] = useState(true)
+const[name, setName] = useState("redColor")
 function toggle(){
   setStyle(!style)
 }
+useEffect(()=>{
+  if(style){
+    setName("redColor")
+  }else{
+    setName("blueColor")
+  }
+},[style])
   return (
     <div id="main">
-      <p className={style ? "redColor" : "blueColor"} >Newton School</p>
+      <p className={name} >Newton School</p>
       <button id='button' onClick={toggle}>Change Style</button>
     </div>
   )
